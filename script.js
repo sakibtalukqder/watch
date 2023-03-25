08903f738183e9cf29903ddaef40952e52edf46d
Sakib Talukqder <126917076+sakibtalukqder@users.noreply.github.com>
2023-03-25 13:33:29 +0600

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,12 @@ function Error(){
   window.alert('Working on it, Try again letter. \nThank you.. :)');
 }
 
+//Sidebar Open & Close
 
-
-function Display(){
-  document.getElementById('mobile').style.display='block';
-  
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
 }
 
-
-
-function Hide(){
-  document.getElementById('mobile').style.display='none';
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
 }
-
-
-
-
